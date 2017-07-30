66ccdb73ee46a42c231966360d167ef6eb6b467b
洗生 <zhaoliang.tzl@alibaba-inc.com>
2017-07-30 21:11:02 +0800

    hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2017 All Rights Reserved.
+ */
+
+/**
+ * @author tzl
+ * @version $Id: HelloWorld.java, v 0.1 2017年07月30日 下午9:08 tzl Exp $
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
